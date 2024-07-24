fbb63a0b3a55e49be850a872011c4f8d93ba2073
kazuaki-ito <kazuaki-ito@isoshi-moustache.com>
2024-07-24 16:56:07 +0900

    commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,42 @@
+<script>
+	import Image from './Image.svelte'
+	let inputValue = '集中力'
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Joke App" />
+</svelte:head>
+
+<section>
+	<h1>
+		Joke App
+	</h1>
+</section>
+<div class="img">
+	<div>
+		何を分けてほしい？
+		<input bind:value={inputValue} type="text" maxlength="3"/>
+	</div>
+	<Image msg={inputValue}/>
+</div>
+
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.img {
+		text-align: center;
+	}
+
+</style>
